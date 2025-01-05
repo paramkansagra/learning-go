150d35beb2cbcafd4fbeebd35a8f5dffcc99da12
aritrika ghosh <141339595+aritrikaghosh@users.noreply.github.com>
2025-01-05 17:53:55 +0530

    Hello world

---
+++ 01-hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+// this is imported automatically so no need to change
+import "fmt"
+
+// go same as C and Java has a single entry point called main
+// so we will make a function main and use the package fmt => formatted IO to print stuff on the screen
+func main() {
+	fmt.Println("Hello world from GO")
+}
